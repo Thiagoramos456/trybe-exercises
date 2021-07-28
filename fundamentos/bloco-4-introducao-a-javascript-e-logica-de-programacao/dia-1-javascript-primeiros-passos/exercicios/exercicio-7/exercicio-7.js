3edd128e8c0f56f21a5da2c4f0787440a44e3cc0
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-07-28 19:28:08 -0300

    (4.1/ex.7) Porcentagem em nta

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios/exercicio-7/exercicio-7.js
@@ -0,0 +1,15 @@
+let porcentagem = 20;
+
+if (porcentagem >= 90){
+    console.log("Nota A");
+} else if (porcentagem >= 80){
+    console.log("Nota B");
+} else if (porcentagem >= 70){
+    console.log("Nota C");
+} else if (porcentagem >= 60){
+    console.log("Nota D");
+} else if (porcentagem >= 50){
+    console.log("Nota E");
+} else{
+    console.log("Nota F")
+}
