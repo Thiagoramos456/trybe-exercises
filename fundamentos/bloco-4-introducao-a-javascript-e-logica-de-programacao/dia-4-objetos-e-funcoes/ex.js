7c3c1581d65893d70aa7ae7209fa06af33599885
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-08-02 18:35:05 -0300

    (4.4/ex.5*)

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/ex.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/ex.js
@@ -22,3 +22,15 @@ for (let propriedade in info){
 for (let propriedade in info){
     console.log(info[propriedade]);
 }
+
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+}
+
+for (propriedade in info){
+    console.log(info[propriedade], "e", info2[propriedade]);
+}
