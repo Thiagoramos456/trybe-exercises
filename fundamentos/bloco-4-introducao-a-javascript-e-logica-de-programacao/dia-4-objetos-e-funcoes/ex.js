75cfc403844d58c7ca6ef28c6a55bc1d6f024ac6
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-08-02 15:56:51 -0300

    (4.4/ex.2*)

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/ex.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/ex.js
@@ -7,6 +7,8 @@ let info = {
   };
 
 // 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
-console.log("Bem-vinda, ", info.personagem);
+console.log('Bem-vinda, ', info.personagem);
 
-// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+//2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+info['recorrente'] = 'Sim';
+console.log(info);
