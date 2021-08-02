3cb218cb069000194c3a9264a4453306706397c4
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-08-02 15:58:20 -0300

    (4.4/ex.3)

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/ex.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/ex.js
@@ -9,6 +9,11 @@ let info = {
 // 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 console.log('Bem-vinda, ', info.personagem);
 
-//2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
 info['recorrente'] = 'Sim';
 console.log(info);
+
+// 3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+for (let propriedade in info){
+    console.log(propriedade);
+}
