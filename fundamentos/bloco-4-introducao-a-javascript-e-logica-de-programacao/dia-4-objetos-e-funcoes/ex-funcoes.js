9a6353fa6d1f2cb8d7442c849bd5317b632673fc
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-08-02 20:10:15 -0300

    (4.4/ex.11*)

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/ex-funcoes.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/ex-funcoes.js
@@ -69,3 +69,12 @@ function inteiroMaisRepetente(numeros){
     return maisRepetente;
 }
 
+// 6 - Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N.
+
+function somatorio(n){
+    let soma = 0;
+    for (let index = 1; index <= n; index += 1){
+        soma += index;
+    }
+    return soma;
+}
