526c81a2ee68054e96a05a78ccbe2d4dd0ac0cbe
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-08-02 23:20:00 -0300

    (4.4/ex.13)

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/ex-funcoes.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/ex-funcoes.js
@@ -1,7 +1,7 @@
 // 1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
 
 function verificaPalindromo(string){
-    stringReversa = string.split("").reverse().join("");
+    let stringReversa = string.split("").reverse().join("");
     return string === stringReversa;
 }
 
