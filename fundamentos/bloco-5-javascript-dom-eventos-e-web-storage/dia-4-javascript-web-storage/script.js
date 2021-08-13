17832a5059b28c553105e8286816bd2bddf7e21f
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-08-13 12:48:38 -0300

    Salvando e recuperando as preferências no Web Storage

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -64,6 +64,7 @@ let fontList = [
 ];
 
 let inputArea = document.getElementById("plain-text");
+let formatedText;
 let submitButton = document.getElementById("btn-submitText");
 let readArea = document.querySelector(".read-area");
 let colorPicker = document.getElementById("color");
@@ -75,15 +76,21 @@ let initialSpaceLine = "50";
 window.onload = init;
 
 function init() {
-  fontSizeSelector.value = initialFontSize;
-  spaceLineSelector.value = initialSpaceLine;
   populateFontList();
+  loadWebStorage();
 
   submitButton.addEventListener("click", readText);
   colorPicker.addEventListener("input", changeColor);
   fontSizeSelector.addEventListener("input", changeFontSize);
   spaceLineSelector.addEventListener("input", changeLineSpacing);
   fontFamilySelector.addEventListener("input", changeFontFamily);
+}
+
+function loadWebStorage() {
+  fontSizeSelector.value = localStorage.getItem("font-size") ? localStorage.getItem("font-size") : initialFontSize;
+  spaceLineSelector.value = localStorage.getItem("line-space") ? localStorage.getItem("line-space") : initialSpaceLine;
+  colorPicker.value = localStorage.getItem("font-color");
+  fontFamilySelector.value = localStorage.getItem("font-family") ? localStorage.getItem("font-family") : fontList[0];
 }
 
 function populateFontList() {
@@ -107,32 +114,47 @@ function createParagraph(text) {
 function readText() {
   let plainText = inputArea.value;
   let paragraph = createParagraph(plainText);
+
   inputArea.remove();
   submitButton.remove();
   readArea.appendChild(paragraph);
+
+  formatedText = document.querySelector(".text");
+  formatedText.style.color = colorPicker.value;
+  formatedText.style.fontSize = fontSizeSelector.value + "px";
+  formatedText.style.lineHeight = spaceLineSelector.value + "px";
+  formatedText.style.fontFamily = fontFamilySelector.value;
+
+  colorPicker.disabled = false;
+  fontSizeSelector.disabled = false;
+  spaceLineSelector.disabled = false;
+  fontFamilySelector.disabled = false;
 }
 
 function changeColor(event) {
   let color = event.target.value;
   let text = readArea.querySelector(".text");
-  console.log(color, text);
   text.style.color = color;
+  localStorage.setItem("font-color", color);
 }
 
 function changeFontSize(event) {
   let fontSize = event.target.value;
   let text = readArea.querySelector(".text");
   text.style.fontSize = fontSize + "px";
+  localStorage.setItem("font-size", fontSize);
 }
 
 function changeLineSpacing(event) {
   let spaceSize = event.target.value;
   let text = readArea.querySelector(".text");
   text.style.lineHeight = spaceSize + "px";
+  localStorage.setItem("line-space", spaceSize);
 }
 
 function changeFontFamily(event) {
   let fontFamily = event.target.value;
   let text = readArea.querySelector(".text");
   text.style.fontFamily = fontFamily;
+  localStorage.setItem("font-family", fontFamily);
 }
