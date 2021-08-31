51768b19cc96e6385ef8e804ea52dcebca47be22
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-08-31 19:08:40 -0300

    (7.3/ex.2.1)

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/Parte 2/addOne.js
@@ -0,0 +1,21 @@
+// 1 - Escreva a função addOne para passar nos testes já implementados.
+
+const assert = require('assert');
+// escreva a função addOne aqui
+const addOne = (numbers) => {
+  const clone = [];
+  for (let index = 0; index < numbers.length; index += 1) {
+    clone.push(numbers[index] + 1);
+  }
+
+  return clone;
+};
+
+const myArray = [31, 57, 12, 5];
+const unchanged = [31, 57, 12, 5];
+const expected = [32, 58, 13, 6];
+const output = addOne(myArray);
+
+assert.strictEqual(typeof addOne, 'function');
+assert.deepStrictEqual(output, expected);
+assert.deepStrictEqual(myArray, unchanged);
