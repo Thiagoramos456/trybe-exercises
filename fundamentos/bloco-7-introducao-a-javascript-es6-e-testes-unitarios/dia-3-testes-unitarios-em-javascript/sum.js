7bdbbf1a9b4ae818007fd143bd33eb35a5d76daa
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-08-31 18:35:36 -0300

    (7.3/ex.1.2)

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/sum.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/sum.js
@@ -18,4 +18,4 @@ function sum(a, b) {
 assert.strictEqual(typeof sum, "function");
 assert.strictEqual(sum(4, 5), 9, "A soma de 4 + 5 deve ser 9.");
 assert.strictEqual(sum(0, 0), 0, "A soma de 0 + 0 deve ser 0.");
-assert.throws(() => {sum(4, "5")});
+assert.throws(() => { sum(4, "5"); }, /^Error: parameters must be numbers/);
