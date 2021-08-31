e2e99b57e1d58566f7b66536eb79de244d324d4e
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-08-31 10:53:17 -0300

    (7.1/ex.2.4)

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2/parte2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte2/parte2.js
@@ -28,21 +28,6 @@ console.log(
   longestWord("Antônio foi no banheiro e não sabemos o que aconteceu")
 ); // retorna 'aconteceu'
 
-// 3 - Crie uma página que contenha:
-// Um botão ao qual será associado um event listener ;
-// Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
-// Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.
-
-let clickCount = 0;
-const btnClick = document.querySelector("button");
-const clickDisplay = document.querySelector("h3");
-
-btnClick.addEventListener("click", () => {
-    console.log("s");
-    clickCount += 1;
-    clickDisplay.innerText = `Clicks: ${clickCount}`;
-});
-
 // 4 - Crie um código JavaScript com a seguinte especificação:
 // Não se esqueça de usar template literals
 // Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
@@ -50,8 +35,26 @@ btnClick.addEventListener("click", () => {
 // String determinada: "Tryber x aqui!"
 // Parâmetro: "Bebeto"
 // Retorno: "Tryber Bebeto aqui!"
+
+const replaceStringByX = (string) => {
+  return `Tryber ${string} aqui!`;
+};
+
+console.log(replaceStringByX("Bebeto"));
+
 // Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
+
+const skills = ["Javascript", "HTML", "CSS", "GIT", "Soft Skills"];
+
 // Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
 // Exemplo: "Tryber x aqui! Minhas cinco principais habilidades são:
 // JavaScript;
 // HTML; ... #goTrybe".
+
+const skillsList = (skills) => {
+  const sortedSkills = skills.sort();
+  const formatedResult = `${replaceStringByX("Thiago")} Minhas cinco principais habilidades são: \n-${sortedSkills.join("\n-")}`;
+  return formatedResult;
+};
+
+console.log(skillsList(skills));
