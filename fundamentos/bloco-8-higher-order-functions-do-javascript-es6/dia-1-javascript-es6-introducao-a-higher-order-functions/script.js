d49e09452fe4c9d3bf491292d23a5ca2a4669aa8
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-09-02 17:59:15 -0300

    (8.1/ex.3)

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -22,12 +22,39 @@ const checkDraw = (num1, num2) => num1 === num2;
 
 const draw = (bet, randomFunction) => {
   const randomNumber = Math.floor(Math.random() * (6 - 1) + 1);
-  console.log(randomNumber)
   if (randomFunction(bet, randomNumber)) {
-      return 'Parabéns, você ganhou';
+    return 'Parabéns, você ganhou';
   } else {
-      return 'Tente novamente'
+    return 'Tente novamente';
   }
 };
 
-console.log(draw(3, checkDraw))
+console.log(draw(3, checkDraw));
+
+// 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+
+const RIGHT_ANSWERS =   ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const calcScore = (actualAnswer, rightAnswer) => {
+  let score = 0;
+
+  for (let index = 0; index < actualAnswer.length; index += 1) {
+    if (actualAnswer[index] === rightAnswer[index]) {
+      score += 1;
+    } else if (rightAnswer[index] === 'N.A') {
+      continue;
+    } else {
+      score -= 0.5;
+    }
+  }
+
+  return score;
+};
+
+const score = (correctAnswers, answers, checkAnswers) => {
+    return checkAnswers(correctAnswers, answers);
+};
+
+console.log(score(RIGHT_ANSWERS, STUDENT_ANSWERS, calcScore));
