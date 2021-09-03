369ebca7c7683f59dc4c9b18976eaa9c8068aae5
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-09-02 21:29:09 -0300

    (7.3/ex.3.2)

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/Parte 3/removeVowels.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/Parte 3/removeVowels.js
@@ -1,26 +1,30 @@
 // 2 - Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
 
+const assert = require('assert');
+
 const removeVowels = (word) => {
-    const characters = word.split('');
-    const vowelConversion = {}
-    const results = [];
-  
-    for (let index = 0; index < characters.length; index += 1) {
-      if (
-        characters[index] === 'a' ||
-        characters[index] === 'o' ||
-        characters[index] === 'i' ||
-        characters[index] === 'e' ||
-        characters[index] === 'u'
-      ) {
-        results.push(characters[index]);
-      } else {
-        results.push('_');
-      }
+  const characters = word.split('');
+  let count = 1;
+  const results = [];
+
+  for (let index = 0; index < characters.length; index += 1) {
+    if (
+      characters[index] === 'a' ||
+      characters[index] === 'o' ||
+      characters[index] === 'i' ||
+      characters[index] === 'e' ||
+      characters[index] === 'u'
+    ) {
+      results.push(count);
+      count += 1;
+    } else {
+      results.push(characters[index]);
     }
-    return results;
-  };
-  
-  
-  const parameter = 'Dayane';
-  const result = 'D1y2n3';
+  }
+  return results.join('');
+};
+
+const parameter = 'Dayane';
+const result = 'D1y2n3';
+
+assert.strictEqual(removeVowels(parameter), result);
