3e0448d88a6e22cf6f5ad5a56c70bf9c68549c94
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-09-09 20:07:00 -0300

    Fix ex.6

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise6.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise6.js
@@ -3,11 +3,20 @@
 const assert = require('assert');
 
 const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
-const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+const grades = [
+  [9, 8, 10, 7, 5],
+  [10, 9, 9, 10, 8],
+  [10, 7, 10, 8, 9],
+];
 
 function studentAverage() {
   // escreva seu código aqui
-  return students.map((student, index) => ({name: student, average: parseFloat((grades[index].reduce((acc, grade) => acc + grade, 0)/grades[index].length).toFixed(1))}));
+  return students.map((student, index) => ({
+    name: student,
+    average:
+      grades[index].reduce((acc, grade) => acc + grade, 0) /
+      grades[index].length,
+  }));
 }
 
 const expected = [
