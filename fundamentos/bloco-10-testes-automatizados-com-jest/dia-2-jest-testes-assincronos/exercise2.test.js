aed153d830c371c4b958358bf0bd6caf8cfcb6f6
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-09-22 21:59:33 -0300

    (10/ex.2)

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/exercise2.test.js
@@ -0,0 +1,35 @@
+// 2 - Utilizando a sintaxe de Promise , faça um teste que verifique o resultado da função getUserName para o caso em que o usuário é encontrado, e também um teste para o caso em que o usuário não é encontrado.
+// Dica: Veja os dados falsos utilizados no banco de dados, disponíveis na variável users , para saber quais IDs existem.
+
+const users = [
+  { id: 1, name: 'Mark' },
+  { id: 2, name: 'Paul' },
+];
+
+const findUserById = (id) =>
+  new Promise((resolve, reject) => {
+    const result = users.find((user) => user.id === id);
+
+    if (result) {
+      return resolve(result);
+    }
+
+    return reject(new Error(`User with ${id} not found.`));
+  });
+
+const getUserName = (userId) => findUserById(userId).then((user) => user.name);
+
+describe('Testando busca de usuário', () => {
+  it('Retornar username caso ele exista', () => {
+    return getUserName(1).then((user) => {
+      expect(user).toEqual('Mark');
+    });
+  });
+
+  it('Retornar erro caso não exista', () => {
+    expect.assertions(1);
+    return getUserName(3).catch((error) => {
+      expect(error.message).toMatch('User with 3 not found.');
+    });
+  });
+});
