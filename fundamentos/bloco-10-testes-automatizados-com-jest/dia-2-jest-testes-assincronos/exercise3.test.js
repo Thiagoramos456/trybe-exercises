df9ad7fd3f03f76ae76f6721e5a40e708e92e46d
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-09-22 22:19:02 -0300

    (10/ex.3)

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/exercise3.test.js
@@ -0,0 +1,40 @@
+// 3 - Reescreva o teste do exercício anterior, desta vez utilizando a sintaxe de async/await .
+
+const users = [
+  { id: 1, name: 'Mark' },
+  { id: 2, name: 'Paul' },
+];
+
+const findUserById = (id) =>
+  new Promise((resolve, reject) => {
+    const result = users.find((user) => user.id === id);
+
+    if (result) {
+      return resolve(result);
+    }
+
+    return reject(new Error(`User with ${id} not found.`));
+  });
+
+const getUserName = (userId) => findUserById(userId).then((user) => user.name);
+
+describe('Testando busca de usuário', () => {
+  it('Retornar username caso ele exista', async () => {
+    try {
+      const data = await getUserName(1);
+      expect(data).toBe('Mark');
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  it('Retornar erro caso não exista', async () => {
+    expect.assertions(1);
+    try {
+      const data = await getUserName(3);
+      console.log(data);
+    } catch (error) {
+      expect(error.message).toMatch('User with 3 not found.');
+    }
+  });
+});
