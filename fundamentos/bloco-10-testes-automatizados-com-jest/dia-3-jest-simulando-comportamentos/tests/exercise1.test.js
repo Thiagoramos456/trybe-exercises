d3732a71373d5b814725188fe78eb736468703e0
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-09-23 21:50:20 -0300

    (10.3/ex.2)

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/tests/exercise1.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/tests/exercise1.test.js
@@ -2,5 +2,8 @@ const randomLib = require('../exercise1.js');
 
 it('Checa se retorna um número aleatório (10)', () => {
   randomLib.random = jest.fn().mockReturnValue(10);
+  
   expect(randomLib.random()).toBe(10);
+  expect(randomLib.random).toHaveBeenCalled();
+  expect(randomLib.random).toHaveBeenCalledTimes(1);
 });
