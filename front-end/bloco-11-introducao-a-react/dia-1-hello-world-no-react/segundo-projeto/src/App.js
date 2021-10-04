eef4a9b5dc07ee6964393bc5418e92e8955386a0
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-10-04 19:31:45 -0300

    (11.1/ex.3)

---
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/segundo-projeto/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './Header.jsx';
+import Content from './Content.jsx'
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Content></Content>
+    </div>
+  );
+}
+
+export default App;
