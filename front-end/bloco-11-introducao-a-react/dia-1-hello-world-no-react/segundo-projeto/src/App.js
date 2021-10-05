a04350121aad44c440bbd4f35ebb2563aac42512
Thiagoramos456 <thiagovirtual10@gmail.com>
2021-10-04 21:12:24 -0300

    Corrigindo footer do ex 3

--- front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/segundo-projeto/src/App.js
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/segundo-projeto/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './Header.jsx';
 import Content from './Content.jsx'
+import Footer from './Footer.jsx';
+
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Content></Content>
+      <Footer></Footer>
     </div>
   );
 }
